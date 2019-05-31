889ff7ed8f3b8a3d5effe3facdb1218b0aec5e03
luizedu9 <39964043+luizedu9@users.noreply.github.com>
2019-05-30 21:47:22 -0300

    def aloca

--- Eventos/Fim_MedicamentosExames.py
+++ Eventos/Fim_MedicamentosExames.py
@@ -6,3 +6,15 @@ class Fim_MedicamentosExames:
     
   def executa():
     pass
+  
+  def aloca_paciente():
+    pass
+  
+  def aloca_atendente():
+    pass
+  
+  def desaloca_paciente():
+    pass
+  
+  def desaloca_atendente():
+    pass
