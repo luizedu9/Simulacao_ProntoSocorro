05d964d2878d2d06c53f45ecdca247664df1180c
Luiz Eduardo Pereira <luizedu_pereira000@hotmail.com>
2019-06-06 16:33:40 -0300

    Event_Listeners Completo

--- Entidades/Enfermeiro.py
+++ Entidades/Enfermeiro.py
@@ -5,9 +5,8 @@ class Enfermeiro:
         self.total_ocioso = 0.0
         self.inicio_horario_ocioso = 0.0
         self.ocupado = False
-        self.evento_atual = None #ponteiro
         
-   # Quando alocar para uma ação, olhar horario atual e o horario em que começou a ficar ocioso 
+    # Quando alocar para uma ação, olhar horario atual e o horario em que começou a ficar ocioso 
     def set_tempo_ocioso(self, horario_atual):
         self.total_ocioso += horario_atual - self.inicio_horario_ocioso
         return
